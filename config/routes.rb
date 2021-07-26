967cc2d7fe7b3c3b384097a24b2d57d015614eaf
shoponcom001 <shopon.com001@gmail.com>
2021-07-26 14:11:16 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
  root to: "homes#top"
  get "/about" => "homes#about"
  devise_for :users
  resources :books, only: [:new, :create, :index, :show, :destroy]
- 
- 
- 
+ resources :users, only: [:show, :edit, :update, :index]
+
+
 end
