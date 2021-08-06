08c429f4188ab4cfd40dcbe37a7de7af692e4861
shoponcom001 <shopon.com001@gmail.com>
2021-08-06 09:30:13 +0000

    非同期

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,17 @@
 class CommentsController < ApplicationController
 
   def create
-    @book = Book.find(params[:id])
+    @book = Book.find(params[:book_id])
     @comment = @book.comments.build(comment_params)
+    @comments = Book.find(params[:book_id]).comments
     @comment.user_id = current_user.id
     @comment.save
   end
 
   def destroy
     @comment = Comment.find(params[:id])
-    comment.destroy
+    @comments = Book.find(params[:book_id]).comments
+    @comment.destroy
   end
 
   private
